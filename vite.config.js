5c074d541a237ba954b37d9dd97e25c7e66d3531
Fred <huertaserick@yahoo.ie>
2024-12-22 21:46:20 +0000

    added base in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/moonway-travel/", // Add this line to set the base path
+});
